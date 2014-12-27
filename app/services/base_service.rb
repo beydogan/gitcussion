246f6909201b62a720931f82f8c1978abc8b5bc3
Mehmet Beydogan <mbbrain@gmail.com>
2014-12-27 17:55:53 +0200

    Update BaseService

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -4,4 +4,19 @@ class BaseService
   def github_api
     Github.new
   end
+
+  private
+
+  def error(message)
+    {
+        message: message,
+        status: :error
+    }
+  end
+
+  def success
+    {
+        status: :success
+    }
+  end
 end
