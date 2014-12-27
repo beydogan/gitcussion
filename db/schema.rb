95e617d3d0ac441ad8828cbe43039f7f7b48b0d9
Mehmet Beydogan <mbbrain@gmail.com>
2014-12-27 13:25:00 +0200

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226202815) do
+ActiveRecord::Schema.define(version: 20141227111428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20141226202815) do
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "github_id"
+    t.string   "avatar_url"
   end
 
   create_table "users", force: true do |t|
