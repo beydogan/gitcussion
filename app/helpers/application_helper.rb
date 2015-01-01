ba26a7727571f92a8d3b7889f22d7da3cd3557be
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-01 22:47:59 +0200

    Add fa_icon helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  include RailsBootstrapHelpers::Helpers::OptionsHelper
+
+  def fa_icon (icon, options = {})
+    options = options.dup
+
+    icon = ERB::Util.html_escape(icon.to_s)
+    append_class!(options, "fa-" + icon)
+
+    if options.delete(:invert)
+      append_class!(options, "icon-white")
+    end
+
+    cls = options[:class]
+
+    "<i class=\"fa #{cls}\"></i>".html_safe
+  end
 end
