6e5aa6d13d08e8ccad291e50426c295f78594f38
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-02 01:39:16 +0200

    Update index design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < ApplicationController
   layout "application_wide", only: [:index]
 
   def index
-    x = repo_path("kod/hds")
+    @top_commented = Repo.
+        select("repos.*, COUNT(repo_id) as comment_count").
+        joins(:comments).
+        group("repos.id").
+        order("comment_count DESC").
+        limit(10)
+
   end
 end
