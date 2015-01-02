f2031075e6e0193f334e78bffeb5568c5185e2b4
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-02 22:50:56 +0200

    Fix markdown error

--- app/services/fetch_repo_readme_service.rb
+++ app/services/fetch_repo_readme_service.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class FetchRepoReadmeService < BaseService
   def call(fullname)
     params = fullname.split("/")
@@ -5,7 +7,8 @@ class FetchRepoReadmeService < BaseService
       res = github_api.repos.contents.readme params[0], params[1]
       readme_base64 = res.content
       decoded = Base64.decode64 readme_base64
-      readme = github_api.markdown.render text: decoded
+      decoded = decoded.force_encoding("UTF-8")
+      readme = github_api.markdown.render text: decoded, mode: "markdown"
     rescue Exception => e
       return error(e.message)
     end
