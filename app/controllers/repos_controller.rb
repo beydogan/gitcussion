3d4fe6a2f03c50f6d9e47cc3091e401656edfa90
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-02 16:57:08 +0200

    Clean up Repo resource

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -6,12 +6,6 @@ class ReposController < ApplicationController
     @repos = SearchRepoService.new.call(@query)[:payload]
   end
 
-  # GET /repos
-  # GET /repos.json
-  def index
-    @repos = Repo.all
-  end
-
   # GET /repos/1
   # GET /repos/1.json
   def show
@@ -29,55 +23,6 @@ class ReposController < ApplicationController
       end
     end
 
-  end
-
-  # GET /repos/new
-  def new
-    @repo = Repo.new
-  end
-
-  # GET /repos/1/edit
-  def edit
-  end
-
-  # POST /repos
-  # POST /repos.json
-  def create
-    @repo = Repo.new(repo_params)
-
-    respond_to do |format|
-      if @repo.save
-        format.html { redirect_to @repo, notice: 'Repo was successfully created.' }
-        format.json { render :show, status: :created, location: @repo }
-      else
-        format.html { render :new }
-        format.json { render json: @repo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /repos/1
-  # PATCH/PUT /repos/1.json
-  def update
-    respond_to do |format|
-      if @repo.update(repo_params)
-        format.html { redirect_to @repo, notice: 'Repo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @repo }
-      else
-        format.html { render :edit }
-        format.json { render json: @repo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /repos/1
-  # DELETE /repos/1.json
-  def destroy
-    @repo.destroy
-    respond_to do |format|
-      format.html { redirect_to repos_url, notice: 'Repo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
