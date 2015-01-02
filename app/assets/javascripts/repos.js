27af7295fbb879df02577c0f8800a900234f3337
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-02 16:55:47 +0200

    Fix turbolinks error

--- app/assets/javascripts/repos.js
+++ app/assets/javascripts/repos.js
@@ -1,7 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+// For turbolinks
 
-$(document).ready(function(){
+var ready = (function(){
     $('.readme-content').readmore();
 });
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
