aae0c03f10b6811f709982efa3407edf61ff8600
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-11 17:31:52 -0500

    fixes redirecting after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   	redirect_to "http://www.gitcussion.com"
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+  end
+
 end
