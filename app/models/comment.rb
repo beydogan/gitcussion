6e6855e2fbd31d6c34c5d3221fb6aaabc9f6a2bd
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2015-01-15 12:38:51 +0200

    Adds callback to comment to send notification mails to mentioned users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,19 @@
 class Comment < ActiveRecord::Base
-  validates :comment, length: { minimum: 5 }
+  validates :comment, length: {minimum: 5}
 
   belongs_to :user
   belongs_to :repo
+
+  after_commit :notify_mentions, :on => [:create, :update]
+
+
+  def notify_mentions
+    return true # Till mail & background job settings done
+    User.where(username: Gitcussion::UsernameExtractor.parse(self.comment)).each do |user|
+      UserMailer.new_mention(user, comment).deliver
+    end
+  end
+
+#   handle_asynchronously
 
 end
