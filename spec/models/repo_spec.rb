69b0ec02612da5cf7eadf2d70e64a60460a4ef7b
Mehmet Beydogan <mbbrain@gmail.com>
2015-02-10 17:13:42 -0600

    Add repo fabricator spec

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Repo, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid conference factory" do
+    repo = Fabricate(:repo)
+    expect(repo).to be_valid
+  end
 end
