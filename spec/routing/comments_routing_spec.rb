cac77899c4b09f6e1d7482eb1bc046d24ce80906
Mehmet Beydogan <mbbrain@gmail.com>
2015-02-10 17:21:46 -0600

    Update comments routing

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -3,32 +3,13 @@ require "rails_helper"
 RSpec.describe CommentsController, :type => :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/comments").to route_to("comments#index")
-    end
-
-    it "routes to #new" do
-      expect(:get => "/comments/new").to route_to("comments#new")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/comments/1").to route_to("comments#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/comments/1/edit").to route_to("comments#edit", :id => "1")
-    end
-
     it "routes to #create" do
-      expect(:post => "/comments").to route_to("comments#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/comments/1").to route_to("comments#update", :id => "1")
+      expect(:post => "/r/ruby/ruby/comments").to route_to("comments#create", repo_id: "ruby/ruby")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/comments/1").to route_to("comments#destroy", :id => "1")
+      pending
+      expect(:delete => "/r/ruby/ruby/comments/1").to route_to("comments#destroy", id: 1, repo_id: "ruby/ruby")
     end
 
   end
