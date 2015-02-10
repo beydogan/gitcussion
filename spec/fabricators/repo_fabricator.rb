79e651b007c22c8320361725ae87ffc3eef03259
Mehmet Beydogan <mbbrain@gmail.com>
2015-02-10 17:13:16 -0600

    Fix repo fabricator

---
+++ spec/fabricators/repo_fabricator.rb
@@ -0,0 +1,9 @@
+Fabricator(:repo) do
+  name { "ruby" }
+  fullname { "ruby/ruby" }
+  homepage { Faker::Internet.url }
+  stars { Random.rand(6)  }
+  watchers { Random.rand(50)  }
+  forks { Random.rand(6)  }
+  so_data { Hash.new }
+end
