022cf719b4f6bfab5a026b193f7cf0e9150aa4e4
Mehmet Beydogan <mbbrain@gmail.com>
2015-02-11 16:46:56 -0600

    Add HomeController spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe HomeController, :type => :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "returns top commented repos" do
+      get :index
+      @repos = [Fabricate(:repo),Fabricate(:repo),Fabricate(:repo)]
+      @top_commented = Repo.top_commented
+      expect(assigns(:top_commented)).to eq(@top_commented)
+    end
   end
 
 end
