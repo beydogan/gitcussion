38f7d755523702ba75a21cb2d3bc3660467e158e
Mehmet Beydogan <mbbrain@gmail.com>
2015-03-26 11:59:50 +0200

    Add description to FetchRepoReadmeService

--- app/services/fetch_repo_readme_service.rb
+++ app/services/fetch_repo_readme_service.rb
@@ -1,3 +1,11 @@
+# FetchRepoReadmeService is used to get a repo's readme by reponame
+# It gets readme from GitHub using octokit gem
+# @example Sample Usage
+#   readme_service =FetchRepoReadmeService.new.call(fullname)
+#   if readme_service[:status] != :error
+#     @readme = readme_service[:payload]
+#   end
+#   
 class FetchRepoReadmeService < BaseService
   def call(fullname)
     begin
