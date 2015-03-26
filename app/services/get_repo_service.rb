54f2191ea1ef0e6e01f6b29376c1557cebf51e8a
Mehmet Beydogan <mbbrain@gmail.com>
2015-03-26 12:02:06 +0200

    Add description to GetRepoService

--- app/services/get_repo_service.rb
+++ app/services/get_repo_service.rb
@@ -1,3 +1,11 @@
+# GetRepoService is used to get repo from GitHub by reponame
+#
+# @example Sample Usage
+# 	repo_service = GetRepoService.new.call(fullname)
+# 	if repo_service[:status] != :error
+# 		@repo = repo_service[:payload]
+# 	end
+#
 class GetRepoService < BaseService
   def call(fullname)
     params = fullname.split("/")
